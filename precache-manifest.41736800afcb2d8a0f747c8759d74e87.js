ad0e8be1d7db4882cb69b6fbd46d1738f1aed4c9
jimMehta <jimit1@gmail.com>
2020-08-10 19:29:39 -0700

    Updates

---
+++ precache-manifest.41736800afcb2d8a0f747c8759d74e87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a92ca977043d8153cb5f32bdca57571",
+    "url": "/githubpages_for_react/index.html"
+  },
+  {
+    "revision": "af13b92fdf72263491a4",
+    "url": "/githubpages_for_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/githubpages_for_react/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/githubpages_for_react/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af13b92fdf72263491a4",
+    "url": "/githubpages_for_react/static/js/main.b7986422.chunk.js"
+  },
+  {
+    "revision": "820984ceedb4aa6ff553",
+    "url": "/githubpages_for_react/static/js/runtime-main.cceb1ebd.js"
+  }
+]);
